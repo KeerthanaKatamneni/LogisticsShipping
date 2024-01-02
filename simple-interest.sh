c4cb9bcdd463704f544ebfae9406cd193346703c
KeerthanaKatamneni <144861737+KeerthanaKatamneni@users.noreply.github.com>
2024-01-02 06:51:57 -0600

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,7 @@
+principle = float(input("Enter the principal: "))
+rate = float(input("Enter the rate of interest: "))
+timeinyr = float(input("Enter the time in years: "))
+## Calculate si
+simple_interest = principle * rate * timeinyr
+## Display the result
+print(f"Simple Interest: {simple_interest} USD")
